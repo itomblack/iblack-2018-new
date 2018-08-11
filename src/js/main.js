5c2476cd080569f123efa020f2bf66d4104a7d2e
itomblack <itomblack@hotmail.co.uk>
2018-08-10 18:06:02 -0700

    content

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,156 @@
 (function () { 'use strict';
 
-  console.log('test1234');
+		// ******************* HOME *************** //
+		// ********** ************* *************** //
+
+		var home_Intro = new Vue({
+			el: "#intro-home",
+			data: {
+				title: 'Designing future brand experiences',
+				subtitle: 'Your strategic design partner, specializing in digital products & customer experience',
+				backImg: 'photos/mountain.png'
+			}
+		});
+
+
+		var home_Process = new Vue({
+			el: "#process",
+			data: {
+				title: 'Process',
+				subtitle: 'Each client and project is unique. Through years learning from smart people at top design firms, I’ve honed a suite of skills which I adapt to each individual need.',
+				phases: [
+					{
+						id: 'open-workshop',
+						phaseNum: '01',
+						phaseTitle: 'Workshops',
+						description: 'Change begins with understanding & idea generation'
+					},
+					{
+						id: 'open-research',
+						phaseNum: '02',
+						phaseTitle: 'Research',
+						description: 'Identifying what needs to change and where to aim'
+					},
+					{
+						id: 'open-strategy',
+						phaseNum: '03',
+						phaseTitle: 'Strategy',
+						description: 'The art of figuring out the best way to get from here to there'
+					},
+					{
+						id: 'open-design',
+						phaseNum: '04',
+						phaseTitle: 'Design',
+						description: 'The blueprints for the future'
+					},
+					{
+						id: 'open-prototyping',
+						phaseNum: '05',
+						phaseTitle: 'Prototyping',
+						description: 'A simulation of change we can test'
+					},
+					{
+						id: 'open-testing',
+						phaseNum: '06',
+						phaseTitle: 'Testing',
+						description: 'Validating our direction, before we’ve gone too far'
+					},
+					{
+						id: 'open-development',
+						phaseNum: '07',
+						phaseTitle: 'Development',
+						description: 'A living, breathing product others can experience'
+					},
+					{
+						id: 'open-hotchocolate',
+						phaseNum: '08',
+						phaseTitle: 'Hot Chocolate',
+						description: 'A delicious beverage'
+					},
+
+				]
+			}
+		});
+
+
+		var home_Process = new Vue({
+			el: "#clients",
+			data: {
+				title: 'Clients',
+				subtitle: 'I\'ve worked with global brands and brand new startups throughout Europe and North America. Someone you’ll recognize, some you won’t. They all had interesting problems to solve.',
+				clients: [
+					{ name: 'AbCellera' },	{ name: 'Arc’teryx' }, { name: 'AstraZeneca' }, { name: 'Bloombridge' }, { name: 'Brompton' },
+					{ name: 'Cancer Research UK' },	{ name: 'Cape & Cape' }, { name: 'Elavon' }, { name: 'Ellwood Atfield' }, { name: 'Extreme Sports' },
+					{ name: 'Finish Line' },	{ name: 'Galeries Lafayette' }, { name: 'Great British Racing' }, { name: 'Green Stone' }, { name: 'Halfords' },
+					{ name: 'Hirepool' },	{ name: 'Hitsource' }, { name: 'Lendlease' }, { name: 'Manchester Airports Group' }, { name: 'Millenium Point' },
+					{ name: 'Modern Dog Magazine' },	{ name: 'Rileys Sports Bars' }, { name: 'Royal Chelsea Hospital' }, { name: 'Tallink' }, { name: 'Telus' },
+					{ name: 'The Hippodrome Casino' },	{ name: 'The Jockey Club' }, { name: 'The NEC Birmingham' }, { name: 'The Racecourse Association' }, { name: 'The Royal British Legion' },
+					{ name: 'Travelaer' },	{ name: 'TravelSupermarket' }, { name: 'Vancity' }, { name: 'Wagamama' }, { name: 'WeWork' },
+					{ name: 'Yocale' }
+				]
+			}
+		});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ******************* Non Content *************** //
+// ********** ************* *************** //
+
+
+		// ********** FADE OUT HERO IMAGE ********** //
+		var fadeOutItem = document.getElementById('fade-img');
+		var photoZIndex = fadeOutItem.style.zIndex;
+
+		window.addEventListener('scroll', function() {
+				var scrolled = window.pageYOffset;
+
+				//for first bit of scroll do all the things
+				if (scrolled <= 1200) {
+						var opacityLevel = 0.3 - (scrolled / 1000);
+						fadeOutItem.style.opacity = opacityLevel;
+
+						//once hidden move to back
+						if ( opacityLevel <= 0 )  {
+							fadeOutItem.style.display = 'none';
+						} 
+
+						// //reshow moving up
+						if ( opacityLevel >= 0 ) {
+							fadeOutItem.style.display = 'block';
+						} 
+				}
+		});
+		// ********** END ********** //
+
+
+
+
+
+
+
+
 
 
 
@@ -8,3 +158,6 @@
 
 
 }()); // end 'use strict'
+
+
+
