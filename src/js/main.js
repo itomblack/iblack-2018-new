3cdb837a92da95143d3d30be1a8e1d66c6799bf5
itomblack <itomblack@hotmail.co.uk>
2018-08-11 12:30:14 -0700

    cv-start

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,9 @@
 (function () { 'use strict';
 
-		// ******************* HOME *************** //
-		// ********** ************* *************** //
+// ******************* HOME *************** //
+// ********** ************* *************** //
 
-		var home_Intro = new Vue({
+		var home_intro = new Vue({
 			el: "#intro-home",
 			data: {
 				title: 'Designing future brand experiences',
@@ -13,11 +13,12 @@
 		});
 
 
-		var home_Process = new Vue({
+		var home_process = new Vue({
 			el: "#process",
 			data: {
 				title: 'Process',
 				subtitle: 'Each client and project is unique. Through years learning from smart people at top design firms, I’ve honed a suite of skills which I adapt to each individual need.',
+				url: '',
 				phases: [
 					{
 						id: 'open-workshop',
@@ -73,11 +74,12 @@
 		});
 
 
-		var home_Process = new Vue({
+		var home_clients = new Vue({
 			el: "#clients",
 			data: {
 				title: 'Clients',
-				subtitle: 'I\'ve worked with global brands and brand new startups throughout Europe and North America. Someone you’ll recognize, some you won’t. They all had interesting problems to solve.',
+				subtitle: 'I work with established global brands and innovative new startups throughout Europe and North America.',
+				url: '',
 				clients: [
 					{ name: 'AbCellera' },	{ name: 'Arc’teryx' }, { name: 'AstraZeneca' }, { name: 'Bloombridge' }, { name: 'Brompton' },
 					{ name: 'Cancer Research UK' },	{ name: 'Cape & Cape' }, { name: 'Elavon' }, { name: 'Ellwood Atfield' }, { name: 'Extreme Sports' },
@@ -91,11 +93,55 @@
 			}
 		});
 
+		var home_experiments = new Vue({
+			el: "#experiments",
+			data: {
+				title: 'Some experimentation',
+				subtitle: 'We all need a chance to explore new ideas. Here are a couple of playthings I hacked together.',
+				url: '',
+				phases: [
+					{
+						phaseNum: 'Music Hack',
+						phaseTitle: 'Blues harp',
+						description: 'The world’s thinnest harmonica',
+						url: 'http://itomblack.github.io/harp/'
+					},
+					{
+						phaseNum: 'Data Viz Hack',
+						phaseTitle: 'Whisky Map',
+						description: 'Locate your ideal single malt whisky',
+						url: 'https://itomblack.github.io/whisky-map/'
+					}
+				]
+			}
+		});
+
+
+		var home_contact = new Vue({
+			el: "#contact",
+			data: {
+				phaseNum: '',
+				title: 'Contact',
+				subtitle: 'I\'d love to chat',
+				url: 'contact.html'
+			}
+		});
 
 
 
 
+// ******************* CV ***************** //
+// ********** ************* *************** //
 
+
+		var home_contact = new Vue({
+			el: "#intro-cv",
+			data: {
+				title: 'Curriculum Vitae.',
+				subtitle: 'Ian Black',
+				blurb: 'I partner with technology focussed companies to identify innovation opportunities and design interactions across their digital and physical brand touchpoints. I achieve this through an adaptable process of strategy, design & testing that delivering meaningful value for both the business and their customers.'
+			}
+		});
 
 
 
@@ -127,7 +173,7 @@
 				var scrolled = window.pageYOffset;
 
 				//for first bit of scroll do all the things
-				if (scrolled <= 1200) {
+				// if (scrolled <= 1200) {
 						var opacityLevel = 0.3 - (scrolled / 1000);
 						fadeOutItem.style.opacity = opacityLevel;
 
@@ -140,7 +186,7 @@
 						if ( opacityLevel >= 0 ) {
 							fadeOutItem.style.display = 'block';
 						} 
-				}
+				// }
 		});
 		// ********** END ********** //
 
